c126e3c735f10a4395401d03de594ec9d2bf0948
AzizRakhim <azizbekrahimjonov80@gmail.com>
2022-04-07 23:28:23 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,139 @@
+// HOVER SECTION START
+
+let elItem = document.querySelectorAll(".main-box");
+let elCount = 0;
+
+elItem.forEach((item) => {
+  item.addEventListener("mouseenter", () => {
+    let elX = item.querySelector(".bx-x");
+    let elCircle = item.querySelector(".bx-circle");
+    if(elCount == 0 && !item.classList.contains("orange")){
+      elX.classList.remove("d-none");
+    }
+    if(elCount == 1 && !item.classList.contains("orange")){
+      elCircle.classList.remove("d-none");
+    } 
+  });
+
+  item.addEventListener("mouseleave", () => {
+    let elX = item.querySelector(".bx-x");
+    let elCircle = item.querySelector(".bx-circle");
+    if(elCount == 0 && !item.classList.contains("orange")){
+      elX.classList.add("d-none");
+    } 
+    if(elCount == 1 && !item.classList.contains("orange")){
+      elCircle.classList.add("d-none");
+    } 
+  });
+});
+
+// HOVER SECTION END
+// CLICK SECTION START
+
+elItem.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    let elX = item.querySelector(".bx-x");
+    let elCircle = item.querySelector(".bx-circle");
+    if(elCount == 0){
+      item.style.background = "#e85737";
+      item.classList.add("orange")
+      elX.style.display = "block";
+      elCount++;
+      elArr[index].name = "x";
+      elArr[index].save = true;
+      calculate();
+    } else{
+      item.style.background = "#ef9b1e";
+      item.classList.add("orange");
+      elCircle.style.display = "block";
+      elCount--;
+      elArr[index].name = "circle";
+      elArr[index].save = true;
+      calculate();
+    }
+  });
+});
+
+// CLICK SECTION END
+// WINNER SECTION START
+
+let elBox = document.querySelector(".end-box");
+let elText = document.querySelector(".end-text");
+
+function calculate(){
+  if(elArr[0].name == "x" && elArr[1].name == "x" && elArr[2].name == "x"){
+    elBox.style.display = "flex";
+    elText.innerText = "Player 1 wins!";
+  } else if(elArr[0].name == "circle" && elArr[1].name == "circle" && elArr[2].name == "circle"){
+    elBox.style.display = "flex";
+    elText.innerText = "Player 2 wins!";
+  } else if(elArr[3].name == "x" && elArr[4].name == "x" && elArr[5].name == "x"){
+    elBox.style.display = "flex";
+    elText.innerText = "Player 1 wins!";
+  } else if(elArr[3].name == "circle" && elArr[4].name == "circle" && elArr[5].name == "circle"){
+    elBox.style.display = "flex";
+    elText.innerText = "Player 2 wins!";
+  } else if(elArr[6].name == "x" && elArr[7].name == "x" && elArr[8].name == "x"){
+    elBox.style.display = "flex";
+    elText.innerText = "Player 1 wins!";
+  } else if(elArr[6].name == "circle" && elArr[7].name == "circle" && elArr[8].name == "circle"){
+    elBox.style.display = "flex";
+    elText.innerText = "Player 2 wins!";
+  } else if(elArr[0].name == "x" && elArr[4].name == "x" && elArr[8].name == "x"){
+    elBox.style.display = "flex";
+    elText.innerText = "Player 1 wins!";
+  } else if(elArr[0].name == "circle" && elArr[4].name == "circle" && elArr[8].name == "circle"){
+    elBox.style.display = "flex";
+    elText.innerText = "Player 2 wins!";
+  } else if(elArr[2].name == "x" && elArr[4].name == "x" && elArr[6].name == "x"){
+    elBox.style.display = "flex";
+    elText.innerText = "Player 1 wins!";
+  } else if(elArr[2].name == "circle" && elArr[4].name == "circle" && elArr[6].name == "circle"){
+    elBox.style.display = "flex";
+    elText.innerText = "Player 2 wins!";
+  } else if(elArr[0].name == "x" && elArr[3].name == "x" && elArr[6].name == "x"){
+    elBox.style.display = "flex";
+    elText.innerText = "Player 1 wins!";
+  } else if(elArr[0].name == "circle" && elArr[3].name == "circle" && elArr[6].name == "circle"){
+    elBox.style.display = "flex";
+    elText.innerText = "Player 2 wins!";
+  } else if(elArr[1].name == "x" && elArr[4].name == "x" && elArr[7].name == "x"){
+    elBox.style.display = "flex";
+    elText.innerText = "Player 1 wins!";
+  } else if(elArr[1].name == "circle" && elArr[4].name == "circle" && elArr[7].name == "circle"){
+    elBox.style.display = "flex";
+    elText.innerText = "Player 2 wins!";
+  } else if(elArr[2].name == "x" && elArr[5].name == "x" && elArr[8].name == "x"){
+    elBox.style.display = "flex";
+    elText.innerText = "Player 1 wins!";
+  } else if(elArr[2].name == "circle" && elArr[5].name == "circle" && elArr[8].name == "circle"){
+    elBox.style.display = "flex";
+    elText.innerText = "Player 2 wins!";
+  }
+}
+
+// WINNER SECTION END
+// RESTART SECTION START
+
+let elEndBtn = document.querySelector(".end-btn");
+
+elEndBtn.addEventListener("click", () => {
+  elBox.style.display = "none";
+
+  elItem.forEach((item) => {
+    let elX = item.querySelector(".bx-x");
+    let elCircle = item.querySelector(".bx-circle");
+    elX.style.display = "none";
+    elCircle.style.display = "none";
+    item.style.background = "#5baec0";
+    item.classList.remove("orange");
+  });
+
+  elArr.forEach((item) => {
+    item.name = "";
+    item.save = false;
+  });
+});
+
+
+
---
+++ js/object.js
@@ -0,0 +1,47 @@
+let elArr = [
+  {
+    id: 1,
+    save: false,
+    icons: ["bx bx-x", "bx bx-circle"]
+  },
+  {
+    id: 2,
+    save: false,
+    icons: ["bx bx-x", "bx bx-circle"]
+  },
+  {
+    id: 3,
+    save: false,
+    icons: ["bx bx-x", "bx bx-circle"]
+  },
+  {
+    id: 4,
+    save: false,
+    icons: ["bx bx-x", "bx bx-circle"]
+  },
+  {
+    id: 5,
+    save: false,
+    icons: ["bx bx-x", "bx bx-circle"]
+  },
+  {
+    id: 6,
+    save: false,
+    icons: ["bx bx-x", "bx bx-circle"]
+  },
+  {
+    id: 7,
+    save: false,
+    icons: ["bx bx-x", "bx bx-circle"]
+  },
+  {
+    id: 8,
+    save: false,
+    icons: ["bx bx-x", "bx bx-circle"]
+  },
+  {
+    id: 9,
+    save: false,
+    icons: ["bx bx-x", "bx bx-circle"]
+  }
+];
